c9c2d7a5f26466b30a57047e0f3e1aa79723084b
AlixMcGill <alixfmcgill@gmail.com>
2025-11-11 13:40:45 -0600

    Hello Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,57 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0,0,width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main() {
+
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Creates window and opengl context
+    GLFWwindow* window = glfwCreateWindow(800, 600,  "LearnOpenGl", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // changes window size when user resizes window
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+    
+    while(!glfwWindowShouldClose(window)) {
+        // input
+        processInput(window);
+
+        // rendering commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // changes window size when user resizes window
+
+    glfwTerminate();
     return 0;
 }
